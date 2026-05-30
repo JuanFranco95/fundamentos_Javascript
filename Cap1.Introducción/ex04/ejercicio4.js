09d9cdf5836f50cea55fea8d1f358ff87caa07f5
santiagolemosr <santyagolemosr@gmail.com>
2026-05-29 20:50:19 -0500

    operadores

---
+++ Cap1.Introducción/ex04/ejercicio4.js
@@ -0,0 +1,25 @@
+ let nombre, edad, dirección, movil, email; // se pueden declarar varias variables del mismo tipo.
+ 
+ // prompt es una palabara reservada en JavaScript que despliega una función y sale un mensaje de tipo alert para el usuario.
+
+ nombre = prompt("Escriba su nombre: ") // pedir datos al usuario.
+ document.writeln("Bienvenido: ", nombre, "<br>"); // muestra el mensaje en la página web.
+ console.log("Bienvenido: ", nombre); 
+ 
+  edad = prompt("Escriba su edad: ") // pedir datos al usuario.
+ document.writeln("Tu edad es: ", edad, "<br>"); 
+ console.log("Tu edad es: ", edad); // muestra el mensaje en la consola del navegador.)
+
+ dirección = prompt("Escriba su dirección: ") // pedir datos al usuario.
+ document.writeln("Tu dirección es: ", dirección, "<br>"); 
+ console.log("Tu dirección es: ", dirección);
+
+ movil = prompt("Escriba su número de móvil: ") // pedir datos al usuario.
+ document.writeln("Tu número de móvil es: ", movil, "<br>"); 
+ console.log("Tu número de móvil es: ", movil);
+
+ email = prompt("Escriba su correo electrónico: ") // pedir datos al usuario.
+ document.writeln("Tu correo electrónico es: ", email, "<br>"); 
+ console.log("Tu correo electrónico es: ", email);
+
+   
---
+++ Cap1.Introducción/ex05/ejercicio5.js
@@ -0,0 +1,65 @@
+// Operadores matemáticos
+
+let a, b;
+let c, d;
+
+let suma, resta, mult, div, residuo, potencia;
+
+// Obtener los datos a traves del usuario
+a = prompt("Ingrese un número: ");
+b = prompt("Ingrese otro numero: ");
+
+// Resultado de las operaciones
+suma = a + b; // Aqui la operación da un error debido a que se concatenan los datos
+document.write("La suma es: ", suma,"<br>");
+console.log("La suma es: ", suma);
+
+resta = a - b;
+document.write("La resta es: ", resta,"<br>");
+console.log("La resta es: ", resta);
+
+mult = a * b;
+document.write("La multiplicación es: ", mult,"<br>");
+console.log("La multiplicación es: ", mult);
+
+div = a / b;
+document.write("La división es: ", div,"<br>");
+console.log("La división es: ", div);
+
+residuo = a % b;
+document.write("El residuo es: ", residuo,"<br>");
+console.log("El residuo es: ", residuo);
+
+potencia = a ** b;
+document.write("La potencia es: ", potencia,"<br>");
+console.log("La potencia es: ", potencia);
+
+//Optener los datos a traves del usuario
+c = parseInt(prompt("Ingrese un número: "));
+d = parseFloat(prompt("Ingrese otro número: "));
+
+suma = c + d
+resta = c - d
+mult = c * d
+div = c / d
+residuo = c % d
+potencia = c ** d
+
+document.writeln("Los resultados de las operaciones son: ",
+     "Suma: ", suma, "<br>",
+     "Resta: ", resta, "<br>",
+     "Multiplicación: ", mult, "<br>",
+     "División: ", div, "<br>",
+     "Residuo: ", residuo, "<br>",
+     "Potencia: ", potencia, "<br>",
+);
+
+console.log("Las operaciones resueltas son: ",
+    "Suma: ", suma,
+    "Resta: ", resta,
+    "Multiplicación: ", mult,
+    "División: ", div,
+    "Residuo: ", residuo,
+    "Potencia: ", potencia,
+    
+)
